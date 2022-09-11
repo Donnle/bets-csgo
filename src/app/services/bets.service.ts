e07a4201de40052a720afdb3e82696b3762d0af0
VadimParpolita <vadim.parpolita@theraven.tech>
2022-09-11 08:56:06 +0300

    refactored and added change bet

--- src/app/services/bets.service.ts
+++ src/app/services/bets.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Bet, Match, Stats, Team} from "../interfaces/interfaces";
 import {BehaviorSubject} from "rxjs";
+import {AdditionalService} from "./additional.service";
 
 @Injectable({
   providedIn: 'root'
@@ -9,25 +10,57 @@ export class BetsService {
   bets$: BehaviorSubject<Bet[]> = new BehaviorSubject<Bet[]>([]);
   stats$: BehaviorSubject<Stats> = new BehaviorSubject<Stats>({won: 0, lost: 0})
 
-  constructor() {
-    const bets = JSON.parse(localStorage.getItem('bets')) || []
+  constructor(private additionalService: AdditionalService) {
+    const bets = this.additionalService.getDataFromLocalStorage('bets') || []
     this.bets$.next(bets)
 
-    const stats = JSON.parse(localStorage.getItem('stats')) || {won: 0, lost: 0}
+    const stats = this.additionalService.getDataFromLocalStorage('stats') || {won: 0, lost: 0}
     this.stats$.next(stats)
   }
 
-  isBetOnThisGame(match: Match): Bet {
-    for (let bet of this.bets$.value) {
+  isBettedOnMatch(match: Match): Bet {
+    const bets = this.bets$.value
+    for (let bet of bets) {
       if (bet.gameId === match?.id) return bet
     }
     return undefined
   }
 
-  betOnGame(team: Team, match: Match) {
-    const newBets = [...this.bets$.value, {gameUrl: match.HLTVLink, gameId: match.id, teamName: team.name}]
-    localStorage.setItem('bets', JSON.stringify(newBets))
-    this.bets$.next(newBets)
+  betOnMatch(team: Team, match: Match) {
+    const bets = this.bets$.value
+    const newBets = [
+      ...bets,
+      {
+        gameUrl: match.HLTVLink,
+        gameId: match.id,
+        teamName: team.name
+      }
+    ]
+    this.setBets(newBets)
   }
 
+  setBets(bets: Bet[]) {
+    this.additionalService.setDataToLocalStorage('bets', bets)
+    this.bets$.next(bets)
+  }
+
+  setStats(stats: Stats) {
+    this.additionalService.setDataToLocalStorage('stats', stats)
+    this.stats$.next(stats)
+  }
+
+  cancelBet(betId: number) {
+    const bets = this.bets$.value
+    let indexOfBet: number;
+    for (let index = 0; index < bets.length; index++) {
+      if (bets[index].gameId === betId) {
+        indexOfBet = index
+      }
+    }
+
+    if (indexOfBet !== -1) {
+      bets.splice(indexOfBet, 1)
+    }
+    this.setBets(bets)
+  }
 }
